004605e21ce458700c9b2ee6be689b20920009fb
TheWildHustle <145122833+TheWildHustle@users.noreply.github.com>
2024-08-29 23:47:00 -0400

    Add Link component import and minor adjustments in index.tsx for enhanced navigation.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import config from '@/config.json';
 import Chat from '@/shared/components/chat/Chat.tsx';
+import { Link } from 'react-router-dom';
 
 export default function () {
   return (
@@ -10,17 +11,15 @@ export default function () {
       <p>
         This is a nostr and cashu powered bitcoin wallet named sat blaster, it enables stream or subscribe to npub while supporting zap splits 
       </p>
-      
-        
-    
       <p>
         <a href="https://github.com/irislib/iris-docs" target="_blank" rel="noopener noreferrer">
           GitHub
         </a>
-      
-        <h3>Chat</h3>
       </p>
+      <h3>Chat</h3>
       <Chat path="apps/chat/chats/create-iris" />
+      <h3>Dashboard</h3>
+      <Link to="/dashboard">Go to Dashboard</Link>
     </div>
   );
 }
